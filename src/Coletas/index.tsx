38b0509c7b14310dd1c8b092a8e1bae2516c3e07
Matheus <matheus.lima@ibbx.tech>
2023-10-09 07:15:59 -0300

    Detete Coleta

--- src/Coletas/index.tsx
+++ src/Coletas/index.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import ReactECharts from "echarts-for-react";
 import {
-  Collect,
   Container,
   GraphicsContainer,
   Sidebar,
@@ -13,21 +12,48 @@ import {
 interface User {
   nome: string;
   id: string;
-  coletas: string[];
+  coletas: Coleta[];
+}
+
+interface Coleta {
+  id: string;
+  value: string;
 }
 
 export function ColetaApp() {
-  const [coletas, setColetas] = useState<string[]>([]);
+  const [coletas, setColetas] = useState<Coleta[]>([]);
   const [novaColeta, setNovaColeta] = useState("");
   const [nome, setNome] = useState("");
   const [id, setId] = useState("");
   const [userInfos, setUserInfos] = useState<User[]>([]);
+  const [expandedEquipment, setExpandedEquipment] = useState<string | null>(
+    null
+  );
 
   const handleAdicionarColeta = () => {
     if (novaColeta !== "") {
-      setColetas([...coletas, novaColeta]);
+      const newColeta: Coleta = {
+        id: Date.now().toString(),
+        value: novaColeta,
+      };
+      setColetas([...coletas, newColeta]);
       setNovaColeta("");
+
       // Atualize a série de dados do gráfico com as coletas atualizadas
+      const options = {
+        xAxis: {
+          type: "category",
+          data: updatedColetas, // Atualize os dados do eixo X com as coletas atualizadas
+        },
+        series: [
+          {
+            data: updatedColetas.map((coleta) => parseInt(coleta)), // Atualize os dados da série com as coletas atualizadas
+            type: "line",
+          },
+        ],
+        // ... Outras configurações do gráfico ...
+      };
+      setOptions(options); // Atualize as opções do gráfico
     }
   };
 
@@ -48,10 +74,63 @@ export function ColetaApp() {
 
     // Atualize a série de dados do gráfico com as coletas atualizadas
     const allColetas = updatedUserInfos.flatMap((user) => user.coletas);
-    setColetas(allColetas);
+    const options = {
+      xAxis: {
+        type: "category",
+        data: allColetas, // Atualize os dados do eixo X com todas as coletas
+      },
+      series: [
+        {
+          data: allColetas.map((coleta) => parseInt(coleta)), // Atualize os dados da série com todas as coletas
+          type: "line",
+        },
+      ],
+      // ... Outras configurações do gráfico ...
+    };
+    setOptions(options); // Atualize as opções do gráfico
   };
 
-  const options = {
+  const handleRemoverColetaUsuario = (
+    userId: number,
+    coletaToDelete: string
+  ) => {
+    const updatedUserInfos = [...userInfos];
+    const userToUpdate = updatedUserInfos[userId];
+
+    // Filter out the coletaToDelete from the user's coletas array
+    userToUpdate.coletas = userToUpdate.coletas.filter(
+      (coleta) => coleta !== coletaToDelete
+    );
+
+    setUserInfos(updatedUserInfos);
+
+    // Update the chart with the updated coletas
+    const allColetas = updatedUserInfos.flatMap((user) => user.coletas);
+    const options = {
+      xAxis: {
+        type: "category",
+        data: allColetas,
+      },
+      series: [
+        {
+          data: allColetas.map((coleta) => parseInt(coleta)),
+          type: "line",
+        },
+      ],
+      // ... Other chart configurations ...
+    };
+    setOptions(options);
+  };
+
+  const handleExpandEquipment = (equipmentName: string) => {
+    if (expandedEquipment === equipmentName) {
+      setExpandedEquipment(null); // Contraia se já estiver expandido
+    } else {
+      setExpandedEquipment(equipmentName); // Expanda se não estiver expandido
+    }
+  };
+
+  const [options, setOptions] = useState({
     title: {
       text: "Gráfico de Coletas",
     },
@@ -68,7 +147,7 @@ export function ColetaApp() {
         type: "line",
       },
     ],
-  };
+  });
 
   return (
     <Container>
@@ -90,25 +169,43 @@ export function ColetaApp() {
           <h2>Equipamentos Adicionados</h2>
           {userInfos.map((userInfo, index) => (
             <UserCard key={index}>
-              <p>{userInfo.nome}</p>
-              <p>ID: {userInfo.id}</p>
-              <h4>Coletas</h4>
-              <ul>
-                {userInfo.coletas.map((coleta, coletaIndex) => (
-                  <li key={coletaIndex}>{coleta}</li>
-                ))}
-              </ul>
-              <div>
-                <input
-                  type="text"
-                  placeholder="Adicionar Coleta"
-                  value={novaColeta}
-                  onChange={(e) => setNovaColeta(e.target.value)}
-                />
-                <button onClick={() => handleAdicionarColetaUsuario(index)}>
-                  Adicionar Coleta
+              <p>
+                <button onClick={() => handleExpandEquipment(userInfo.nome)}>
+                  {userInfo.nome}
                 </button>
-              </div>
+              </p>
+              {expandedEquipment === userInfo.nome && (
+                <div>
+                  <p>ID: {userInfo.id}</p>
+                  <h4>Coletas</h4>
+                  <ul>
+                    {/* Map over coletas and display them */}
+                    {userInfo.coletas.map((coleta, coletaIndex) => (
+                      <li key={coletaIndex}>
+                        {coleta} {/* Display the coleta value */}
+                        <button
+                          onClick={() =>
+                            handleRemoverColetaUsuario(index, coleta)
+                          }
+                        >
+                          Delete
+                        </button>
+                      </li>
+                    ))}
+                  </ul>
+                  <div>
+                    <input
+                      type="text"
+                      placeholder="Adicionar Coleta"
+                      value={novaColeta}
+                      onChange={(e) => setNovaColeta(e.target.value)}
+                    />
+                    <button onClick={() => handleAdicionarColetaUsuario(index)}>
+                      Adicionar Coleta
+                    </button>
+                  </div>
+                </div>
+              )}
             </UserCard>
           ))}
         </UserInfo>
